6398a15cfce9decd921274a7f9fc6eca2e180168
husswin <huss4in7@outlook.com>
2021-12-04 01:48:32 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@ void setup()
     Serial.begin(115200);
 
     // Initialize the processes
-    Lists::Queue<CPU::Process> processes;
+    DataStructure::Queue<CPU::Process> processes;
 
     // Set sample processes
-    processes.push(CPU::Process(1, 0, 5, 3));
-    processes.push(CPU::Process(2, 3, 10, 4));
-    processes.push(CPU::Process(3, 7, 5, 2));
-    processes.push(CPU::Process(4, 8, 2, 5));
-    processes.push(CPU::Process(7, 9, 4, 1));
-    processes.push(CPU::Process(10, 9, 4, 1));
+    processes.push(CPU::Process(0, 5, 3));
+    processes.push(CPU::Process(3, 10, 4));
+    processes.push(CPU::Process(7, 5, 2));
+    processes.push(CPU::Process(8, 2, 5));
+    processes.push(CPU::Process(9, 4, 1));
+    processes.push(CPU::Process(9, 4, 1));
 
     // Preform the Algorithm and display the results
     CPU::Display::display(CPU::Algorithm::FCFS, processes);
